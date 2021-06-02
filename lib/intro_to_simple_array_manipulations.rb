70f51f41f2d6fe63ef7a6358d186e36ef8ad35f8
Jonathan Yu <jonathan.yu.jwy2@yale.edu>
2021-06-02 19:18:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,6 +3,11 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  array.insert(3, element)
-  return array
+  return array.insert(3, element)
 end
+
+def using_uniq(array)
+  return array.uniq
+end
+
+def using_flatten
