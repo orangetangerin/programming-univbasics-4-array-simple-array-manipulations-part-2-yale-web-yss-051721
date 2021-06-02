1de51f7dd663696a3459bdbf3566e7c286306679
Jonathan Yu <jonathan.yu.jwy2@yale.edu>
2021-06-02 19:22:37 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(array1, array2)
+  return array1.concat(array2)
+end
+
+def using_insert(array, element)
+  return array.insert(4, element)
+end
+
+def using_uniq(array)
+  return array.uniq
+end
+
+def using_flatten(array)
+  return array.flatten
+end
+
+def using_delete(array, string)
+  return array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  return array.delete_at(integer)
+end
